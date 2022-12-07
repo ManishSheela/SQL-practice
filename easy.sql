4d080092c5b6fd77e2eab6b2b86e5c49ed2dc1cf
Manish <96982207+ManishSheela@users.noreply.github.com>
2022-12-07 15:38:29 +0530

    Update easy.sql

--- easy.sql
+++ easy.sql
@@ -1,50 +1,45 @@
-1. Show first name, last name, and gender of patients who's gender is 'M' 
-
+ ** Question 1. Show first name, last name, and gender of patients who's gender is 'M' **
 select first_name,last_name,gender
-from patients where gender = 'M';
+from patients where gender = 'M'
 
-2. Show first name and last name of patients who does not have allergies. (null) 
-
+ ** Question 1. Show first name and last name of patients who does not have allergies. (null) **
 select first_name,last_name
 from patients where allergies IS NULL;
 
-3. Show first name of patients that start with the letter 'C' 
-
+ ** Question 1. Show first name of patients that start with the letter 'C' **
 select first_name from patients
 where first_name like 'C%'
 
-4. Show first name and last name of patients that weight within the range of 100 to 120 (inclusive) 
-
+ ** Question 1. Show first name and last name of patients that weight within
+the range of 100 to 120 (inclusive) **
 select first_name,last_name from patients
 where weight between 100 and 120
 
 select first_name,last_name from patients
 where weight>=100 and weight<=120
 
-5. Update the patients table for the allergies column. If the patient's allergies is null then replace it with 'NKA'
-
+ ** Question 1. Update the patients table for the allergies column. 
+If the patient's allergies is null then replace it with 'NKA'**
 update patients
 set allergies = 'NKA'
 where allergies is NULL
 
-6. Show first name and last name concatinated into
-one column to show their full name. 
+ ** Question 1. Show first name and last name concatinated into
+one column to show their full name. **
 select concat(first_name,' ',last_name) as full_name
 from patients
 
 select (first_name || ' ' || last_name) as full_name
 from patients
 
-7. Show first name, last name, and the full province name of each patient.
-Example: 'Ontario' instead of 'ON'
-
+ ** Question 1. Show first name, last name, and the full province name of each patient.
+Example: 'Ontario' instead of 'ON'**
 select first_name,last_name,province_name
 from patients
 join province_names
 on patients.province_id = province_names.province_id;
 
-8. Show how many patients have a birth_date with 2010 as the birth year.
-
+ ** Question 1. Show how many patients have a birth_date with 2010 as the birth year.**
 select count(year(birth_date)) as total_patients
 from patients
 where year(birth_date) = 2010
@@ -57,8 +52,7 @@ select count(birth_date) as total_patients
 from patients
 where birth_date>= '2010-01-01' and birth_date<= '2010-12-31'; 
 
-9. Show the first_name, last_name, and height of the patient with the greatest height.
-
+ ** Question 1. Show the first_name, last_name, and height of the patient with the greatest height.**
 select first_name,last_name, max(height) as height
 from patients;
 
@@ -70,33 +64,25 @@ select first_name,last_name,height
 from patients
 where height = (select max(height) from patients)
 
-10. Show the total number of admissions for patient_id 579 
+ ** Question 1. Show the total number of admissions for patient_id 579 **
 select patient_id ,count(*)as total_admissions 
 from admissions
 where patient_id = 579
 
-11. Based on the cities that our patients live in, show unique cities that are in province_id 'NS'? 
-
+ ** Question 1. Based on the cities that our patients live in, show unique cities that are in province_id 'NS'? **
 select distinct(city) from patients
 where province_id is 'NS'
 
-12. Write a query to find the first_name, last name and birth date 
-of patients who have height more than 160 and weight more than 70
-
+ ** Question 1. Write a query to find the first_name, last name and birth date of patients who have height more than 160 and weight more than 70**
 select first_name,last_name,birth_date from patients
 where height > 160 and weight > 70
 
-13. Write a query to find list of patients first_name, last_name, and allergies 
-from Hamilton where allergies are not null
-
+ ** Question 1. Write a query to find list of patients first_name, last_name, and allergies from Hamilton where allergies are not null**
 select first_name,last_name,allergies
 from patients
 where city = 'Hamilton' and allergies is not NULL
 
-14. Based on cities where our patient lives in, 
-write a query to display the list of unique city starting with 
-a vowel (a, e, i, o, u). Show the result order in ascending by city.
-
+ ** Question 1. Based on cities where our patient lives in, write a query to display the list of unique city starting with a vowel (a, e, i, o, u). Show the result order in ascending by city.**
 select distinct city from patients
 where city like 'a%' or 
 	  city like 'e%' or 
