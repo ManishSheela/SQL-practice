cfaa0a87469a5765302b52cf2eef382218e0b31d
Manish <96982207+ManishSheela@users.noreply.github.com>
2022-12-07 15:48:14 +0530

    Update and rename easy.sql to easy.md

--- easy.sql
+++
@@ -1,6 +0,0 @@
- ** Question 1. Show first name and last name concatinated into
-one column to show their full name. **
-```
-select concat(first_name,' ',last_name) as full_name
-from patients
-```
