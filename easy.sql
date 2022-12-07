02f0657a6b789c1e997dac4ce0c2ee7c11087299
Manish <96982207+ManishSheela@users.noreply.github.com>
2022-12-07 12:01:55 +0530

    Update easy.sql

--- easy.sql
+++ easy.sql
@@ -1,9 +1,9 @@
-1.  Show first name, last name, and gender of patients who's gender is 'M' */
+/* Show first name, last name, and gender of patients who's gender is 'M' */
 
-```{sql}
+
 select first_name,last_name,gender
 from patients where gender = 'M';
-```
+
 
 /* Show first name and last name of patients who does not have allergies. (null) */
 select first_name,last_name
